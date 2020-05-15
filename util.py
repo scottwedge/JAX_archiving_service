ab303675281b7e2bdb6bbfb6ef7e7dccc85f29ac
Frank <52245296+zappuf@users.noreply.github.com>
2020-05-15 18:14:56 -0400

    updated some utils and updated logger

---
+++ util.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from inspect import currentframe
+from inspect import getframeinfo
+from logger import LOGGER
+import inspect
+import smtplib
+import sys
+
+
+body = (
+    f"Hi {user['fname'].capitalize()},\nYour request to {action} {source_path} has "
+    f"{'completed successfully' if status else 'failed'}.\n"
+)
+
+
+def test_email_body():
+    return "This is a boring test string for the email.\nIncluding a newline for fun."
+
+
+def send_email(recipients, body, subject="Test Email", to="frank zappulla"):
+    msg = MIMEMultipart()
+    # Who recipient(s) will see the email is from
+    msg["From"] = "JAX Archiver <noreply-archiver@jax.org>"
+    # Who the recipient(s) visually see the email is addressed to
+    msg["To"] = to
+    # Text in subject line
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+    msg_text = msg.as_string()
+    try:
+        with smtplib.SMTP("smtp.jax.org", 25) as server:
+            # 1st arg overwritten by msg["From"] above
+            # 2nd arg is list of recipients and who actually receives the email
+            # 3rd arg body of email
+            # result will return an empty dict when server does not refuse any
+            # recipient. Returns dict of refused emails otherwise
+            result = server.sendmail("return-email@jax.org", recipients, msg_text)
+            if result:
+                err_msg = f"ERROR: The following recipients were refused, {result}"
+                # LOGGER.error(err_msg)
+                sys.stderr.write(err_msg)
+    except Exception as e:
+        # LOGGER.debug(f"error sending email: {e}")
+        pass
+
+
+# def send_err_email(body):
+#     send_email(
+#         config.err_email_list,
+#         body,
+#         subject="Archiver Error",
+#         to="Data-Services@jax.org",
+#     )
+#     return
+
+
+def log_email(msg, error=False):
+    ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    filename = getframeinfo(currentframe()).filename
+    lineno = inspect.currentframe().f_back.f_lineno
+    log_prefix = f"[{ts}, {filename}:{lineno}]"
+    log_msg = f"{log_prefix} {msg}"
+    if error:
+        log_msg = f"{log_prefix} ERROR: {msg}"
+        sys.stderr.write(f"{log_msg}\n")
+        # send email here
+        # send_err_email(body)
+        LOGGER.error(log_msg)
+    else:
+        LOGGER.info(log_msg)
+    print(log_msg)
+    return
